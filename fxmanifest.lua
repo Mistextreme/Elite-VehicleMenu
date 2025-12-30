38756e33619b07abfad6d1cc8ef5ef83a42e1f91
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-30 23:36:47 +0000

    Upload: fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,40 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+version '1.0.4'
+author 'OffSey | Elite Store'
+description 'Elite Vehicle Menu'
+
+shared_script {
+  'config.lua',
+  'locales.lua'
+}
+
+client_scripts { 
+  'locales.lua',
+  'client/cl_utils.lua',
+  'client/client.lua'
+}
+
+server_scripts { 
+  'server/server.lua',
+  'server/bridge/*.lua'
+}
+
+ui_page 'web/index.html'
+
+files {
+  'web/index.html',
+  'web/style.css',
+  'web/scripts.js',
+  'web/img/*.png',
+  'web/img/**/*.png'
+}
+
+escrow_ignore {
+  'config.lua',
+  'locales.lua',
+  'client/cl_utils.lua',
+  'server/bridge/*.lua'
+}
+dependency '/assetpacks'
