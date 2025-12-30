e27bc02838430fe463504c66701ab7db7491e702
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-30 23:39:06 +0000

    Eliminar cl_utils.lua

--- cl_utils.lua
+++
@@ -1,9 +0,0 @@
-utils = {}
-
-function utils.GiveCarKeys(vehicle, vehiclePlate)
-    -- print(vehicle, vehiclePlate)
-end
-
-function utils.RemoveCarKeys(vehicle, vehiclePlate)
-    -- print(vehicle, vehiclePlate)
-end
