5f7cfb28cef692d726fa556321c93749eb411a9b
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-30 23:36:48 +0000

    Upload: locales.lua

---
+++ locales.lua
@@ -0,0 +1,69 @@
+Locales = {
+  en = {
+    vm_title = "Vehicle Menu",
+    vm_selected = "Selected",
+    vm_none = "None",
+    vm_category = "Category",
+    vm_all = "All",
+    vm_favorites = "Favorites",
+    vm_plate = "Plate",
+    vm_random = "Random",
+
+    vm_search_placeholder = "Search vehicles...",
+    vm_no_vehicles = "No vehicles",
+
+    vm_pick_color = "Pick Color",
+    vm_random_color = "Random color",
+    vm_rgb_format = "RGB(%d, %d, %d)",
+
+    vm_custom_plate = "Custom Plate",
+    vm_plate_input_placeholder = "PLATE",
+    vm_plate_random_btn = "Random plate",
+    vm_plate_counter_format = "%d/8",
+
+    vm_plate_style_plate01 = "Blue/White 1",
+    vm_plate_style_plate02 = "Blue/White 2",
+    vm_plate_style_plate03 = "Blue/White 3",
+    vm_plate_style_plate04 = "Yellow/Blue",
+    vm_plate_style_plate05 = "Yellow/Black",
+    vm_plate_style_yankton = "North Yankton",
+
+    vm_spawn = "Spawn",
+    vm_random_vehicle = "Random",
+    vm_close = "Close",
+  },
+
+  fr = {
+    vm_title = "Menu Véhicule",
+    vm_selected = "Sélection",
+    vm_none = "Aucun",
+    vm_category = "Catégorie",
+    vm_all = "Tous",
+    vm_favorites = "Favoris",
+    vm_plate = "Plaque",
+    vm_random = "Aléatoire",
+
+    vm_search_placeholder = "Rechercher un véhicule…",
+    vm_no_vehicles = "Aucun véhicule",
+
+    vm_pick_color = "Choisir une couleur",
+    vm_random_color = "Couleur aléatoire",
+    vm_rgb_format = "RGB(%d, %d, %d)",
+
+    vm_custom_plate = "Plaque personnalisée",
+    vm_plate_input_placeholder = "PLATE",
+    vm_plate_random_btn = "Plaque aléatoire",
+    vm_plate_counter_format = "%d/8",
+
+    vm_plate_style_plate01 = "Bleu/Blanc 1",
+    vm_plate_style_plate02 = "Bleu/Blanc 2",
+    vm_plate_style_plate03 = "Bleu/Blanc 3",
+    vm_plate_style_plate04 = "Jaune/Bleu",
+    vm_plate_style_plate05 = "Jaune/Noir",
+    vm_plate_style_yankton = "North Yankton",
+
+    vm_spawn = "Apparaître",
+    vm_random_vehicle = "Aléatoire",
+    vm_close = "Fermer",
+  }
+}
