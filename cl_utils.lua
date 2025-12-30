c68862e7c958b4cc8d787f5ea0e7019045c05beb
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-30 23:36:49 +0000

    Upload: cl_utils.lua

---
+++ cl_utils.lua
@@ -0,0 +1,9 @@
+utils = {}
+
+function utils.GiveCarKeys(vehicle, vehiclePlate)
+    -- print(vehicle, vehiclePlate)
+end
+
+function utils.RemoveCarKeys(vehicle, vehiclePlate)
+    -- print(vehicle, vehiclePlate)
+end
